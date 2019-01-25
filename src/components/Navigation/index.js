5b1a663c2a6b6f496bbee5b6c437f38d1ef503ba
luckychips <sksms20000@gmail.com>
2019-01-25 15:34:43 +0900

    change material ui component in password forget page.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -17,7 +17,6 @@ const Navigation = () => (
 
 const NavigationAuth = () => (
     <ul>
-        <li><Link to={ROUTES.LANDING}>Landing</Link></li>
         <li><Link to={ROUTES.HOME}>Home</Link></li>
         <li><Link to={ROUTES.ACCOUNT}>Account</Link></li>
         <li><Link to={ROUTES.ADMIN}>Admin</Link></li>
@@ -27,7 +26,6 @@ const NavigationAuth = () => (
 
 const NavigationNonAuth = () => (
     <ul>
-        <li><Link to={ROUTES.LANDING}>Landing</Link></li>
         <li><Link to={ROUTES.SIGN_IN}>Sign In</Link></li>
     </ul>
 );
--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { withStyles } from "@material-ui/core";
+import {Button, TextField, withStyles} from "@material-ui/core";
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
@@ -10,15 +10,35 @@ const styles = theme => ({
         'max-width': 450,
         'margin': '0 auto',
         'padding': '10px 0 5px 0'
+    },
+    container: {
+        'display': 'flex',
+        'flexWrap': 'wrap',
+        'max-width': 450,
+        'margin': '0 auto'
+    },
+    title: {
+        'text-align': 'center',
+        'font-weight': 100
+    },
+    button: {
+        'width': '100%',
+        'margin-top': 20
     }
 });
 
-const PasswordForgetPage = () => (
-    <div>
-        <h1>PasswordForget</h1>
-        <PasswordForgetForm />
-    </div>
-);
+class PasswordForgetPage extends Component {
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <div>
+                <h1 className={classes.title}>Forgot Password</h1>
+                <PasswordForgetForm />
+            </div>
+        );
+    }
+}
 
 class PasswordForgetLinkBase extends Component {
     render() {
@@ -65,21 +85,31 @@ class PasswordForgetFormBase extends Component {
 
     render() {
         const { email, error } = this.state;
+        const { classes } = this.props;
 
         const isInvalid = email === '';
 
         return (
-            <form onSubmit={this.onSubmit}>
-                <input
+            <form onSubmit={this.onSubmit} className={classes.container}>
+                <TextField
+                    id="password-forget-email-field"
+                    label="Email"
                     name="email"
-                    value={this.state.email}
-                    onChange={this.onChange}
                     type="text"
+                    value={this.state.email}
                     placeholder="Email Address"
+                    helperText=""
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true
+                    }}
+                    onChange={this.onChange}
                 />
-                <button disabled={isInvalid} type="submit">
+
+                <Button variant="outlined" color="primary" className={classes.button} disabled={isInvalid} type="submit">
                     Reset My Password
-                </button>
+                </Button>
 
                 {error && <p>{error.message}</p>}
             </form>
@@ -87,8 +117,8 @@ class PasswordForgetFormBase extends Component {
     }
 }
 
-const PasswordForgetForm = withFirebase(PasswordForgetFormBase);
+const PasswordForgetForm = withFirebase(withStyles(styles)(PasswordForgetFormBase));
 const PasswordForgetLink = withStyles(styles)(PasswordForgetLinkBase);
 
 export { PasswordForgetForm, PasswordForgetLink };
-export default PasswordForgetPage;
+export default withStyles(styles)(PasswordForgetPage);
